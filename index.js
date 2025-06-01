c6a32a23a6444894c74bae1aa1aefcee95d99164
Abhijeetrauthan111111 <abhijeetrauthan0@gmail.com>
2025-06-01 15:19:07 +0530

    fix: correct server start script in package.json and update path resolution in index.js

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ import eventRoute from "./routes/event.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./socket/socket.js";
 import path from "path";
- 
-dotenv.config();
+import { fileURLToPath } from 'url';
 
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //middlewares
 app.use(express.json());
@@ -33,10 +34,11 @@ app.use("/api/v1/message", messageRoute);
 app.use("/api/v1/event", eventRoute);
 
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-app.get("*", (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-})
+app.use(express.static(path.join(__dirname, "..", "..", "frontend", "dist")));
+app.get("*", (req, res) => {
+  if (req.url.startsWith('/api')) return next();
+  res.sendFile(path.join(__dirname, "..", "..", "frontend", "dist", "index.html"));
+});
 
 
 server.listen(PORT, () => {
